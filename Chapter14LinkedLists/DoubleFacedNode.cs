7305431d1c6ca3497f39d5a0a8df975a930837ba
Hafiz Adewuyi <hoadewuyi@gmail.com>
2021-05-10 21:33:16 +0300

    Add DoublyLinkedList

---
+++ Chapter14LinkedLists/DoubleFacedNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonSenseGuideDataStructuresAlgorithms.Chapter14LinkedLists
+{
+    public class DoubleFacedNode : ListNode
+    {
+        public ListNode PreviousNode { get; set; }
+
+        public DoubleFacedNode(string data) : base(data)
+        {
+        }
+    }
+}
---
+++ Chapter14LinkedLists/DoublyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonSenseGuideDataStructuresAlgorithms.Chapter14LinkedLists
+{
+    public class DoublyLinkedList : LinkedList
+    {
+        public ListNode LastNode { get; set; }
+
+        public DoublyLinkedList(DoubleFacedNode firstNode = null, DoubleFacedNode lastNode = null) : base(firstNode)
+        {
+            LastNode = lastNode;
+        }
+
+        public void InsertAtEnd(string data)
+        {
+            var newNode = new DoubleFacedNode(data);
+
+            // If there are no elements yet in the linked list:
+            if (FirstNode == null)
+            {
+                FirstNode = newNode;
+                LastNode = newNode;
+            }
+            else //If the linked list already has at least one node:
+            {
+                newNode.PreviousNode = LastNode;
+                LastNode.NextNode = newNode;
+                LastNode = newNode;
+            }
+        }
+    }
+}
